ccf7f0e1b6eb1aac2d19bbd5a0a2e041dc427512
yueyu <yue.yu@changhong.com>
2018-02-06 14:51:20 +0800

    Initial commit

---
+++ src/main/java/com/bigdata/recomnd/rankusercount/RankuserCountApplication.java
@@ -0,0 +1,12 @@
+package com.bigdata.recomnd.rankusercount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankuserCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankuserCountApplication.class, args);
+	}
+}
